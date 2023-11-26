5ece309a519b60ceacff2fa62d7b6a6f6bec08fd
Harris <harrisjose@outlook.com>
2023-11-26 16:00:45 +0530

    Initial commit
    + scaffold tauri-app with vite and react
    + change app to system-tray only

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pulse
+
+wip
+
+## References
+- Create a Menu Bar App With Tauri   
+    - link: https://betterprogramming.pub/create-menubar-app-with-tauri-510ab7f7c43d   
+    - github: https://github.com/4gray/tauri-menubar-app
+
+- Only tray icon, no window   
+    https://github.com/tauri-apps/tauri/discussions/2684
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,64 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#[cfg(target_os = "macos")]
+use tauri::CustomMenuItem;
+use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
+    let system_tray_menu = SystemTrayMenu::new().add_item(quit);
+
+    tauri::Builder::default()
+        .setup(|app| {
+            app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_positioner::init())
+        .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .on_system_tray_event(|app, event| match event {
+                SystemTrayEvent::LeftClick {
+                    position: _,
+                    size: _,
+                    ..
+                } => {
+                    let window = app.get_window("main").unwrap();
+                    // toggle application window
+                    if window.is_visible().unwrap() {
+                        window.hide().unwrap();
+                    } else {
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                },
+                SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        )
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::Focused(is_focused) => {
+                // detect click outside of the focused window and hide the app
+                if !is_focused {
+                    event.window().hide().unwrap();
+                }
+            }
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
