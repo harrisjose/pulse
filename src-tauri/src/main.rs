b341ef4d27d9c72531bccd51201a23edaf028f51
harris <harrisjose@outlook.com>
2023-11-27 10:08:00 +0530

    Scaffold ui
    + tailwind
    + svgr
    + radix-ui/switch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,58 +1,88 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#[cfg(target_os = "macos")]
-use tauri::CustomMenuItem;
-use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+#![cfg(target_os = "macos")]
+use serde::{Deserialize, Serialize};
+use std::{fs, path::PathBuf};
+use tauri::{Manager, SystemTray, SystemTrayEvent};
+use tauri_plugin_positioner::{Position, WindowExt};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[derive(Serialize, Deserialize)]
+struct Config {
+    connectors: ConnectorsConfig,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ConnectorsConfig {
+    api: Option<ApiConnectorConfig>,
+    slack: Option<SlackConnectorConfig>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ApiConnectorConfig {
+    read_url: String,
+    write_url: String,
+    data: serde_json::Value,
+    enabled: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SlackConnectorConfig {
+    // Example:
+    token: String,
+    enabled: bool,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_config() -> Result<Config, String> {
+    let mut config_path = PathBuf::from(std::env::var("HOME").map_err(|e| e.to_string())?);
+    config_path.push(".pulse.json");
+
+    let config_str = fs::read_to_string(config_path).map_err(|e| e.to_string())?;
+
+    serde_json::from_str(&config_str).map_err(|e| e.to_string())
 }
 
 fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
-    let system_tray_menu = SystemTrayMenu::new().add_item(quit);
-
     tauri::Builder::default()
         .setup(|app| {
             app.set_activation_policy(tauri::ActivationPolicy::Accessory);
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_config])
         .plugin(tauri_plugin_positioner::init())
-        .system_tray(SystemTray::new().with_menu(system_tray_menu))
-        .on_system_tray_event(|app, event| match event {
+        .system_tray(SystemTray::new())
+        .on_system_tray_event(|app, event| {
+            tauri_plugin_positioner::on_tray_event(app, &event);
+            match event {
                 SystemTrayEvent::LeftClick {
                     position: _,
                     size: _,
                     ..
                 } => {
                     let window = app.get_window("main").unwrap();
-                    // toggle application window
+                    let _ = window.move_window(Position::TrayCenter);
+
                     if window.is_visible().unwrap() {
                         window.hide().unwrap();
                     } else {
                         window.show().unwrap();
                         window.set_focus().unwrap();
                     }
-                },
+                }
                 SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                    "quit" => {
-                        std::process::exit(0);
-                    }
+                    "quit" => app.exit(0),
                     _ => {}
                 },
                 _ => {}
             }
-        )
+        })
         .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::Focused(is_focused) => {
-                // detect click outside of the focused window and hide the app
-                if !is_focused {
-                    event.window().hide().unwrap();
-                }
-            }
+            // tauri::WindowEvent::Focused(is_focused) => {
+            //     // detect click outside of the focused window and hide the app
+            //     if !is_focused {
+            //         event.window().hide().unwrap();
+            //     }
+            // }
             tauri::WindowEvent::CloseRequested { api, .. } => {
                 event.window().hide().unwrap();
                 api.prevent_close();
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import * as SwitchPrimitives from "@radix-ui/react-switch";
+import SlackLogo from "./assets/slack.svg";
+import PersonalLogo from "./assets/at-sign.svg";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-black min-h-[200px] h-full w-full border rounded-md border-white border-opacity-20 text-white p-3 flex flex-col space-y-3">
+      <div className="flex justify-between mb-2">
+        <div className="font-semibold text-sm">Available</div>
+        <SwitchPrimitives.Root
+          className={
+            "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input"
+          }
+        >
+          <SwitchPrimitives.Thumb
+            className={
+              "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0"
+            }
+          />
+        </SwitchPrimitives.Root>
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <div className="flex items-center">
+        <div className="bg-slate-800 rounded-full p-1 flex justify-center items-center mr-2 bg-primary">
+          <SlackLogo className="w-4 h-3.5"></SlackLogo>
+        </div>
+        <div className="font-light text-sm">Slack</div>
+      </div>
+      <div className="flex items-center">
+        <div className="bg-slate-800 rounded-full p-1 flex justify-center items-center mr-2 bg-primary">
+          <PersonalLogo className="w-4 h-3.5"></PersonalLogo>
+        </div>
+        <div className="font-light text-sm">Personal</div>
+      </div>
     </div>
   );
 }
